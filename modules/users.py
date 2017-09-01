c942e941121d81c0c62c5ad4fbf5473741d710a1
Draku <vladimir.ibazeta@gmail.com>
2017-09-01 18:17:38 -0500

    Vista de registro de cuenta

--- modules/users.py
+++ modules/users.py
@@ -4,13 +4,35 @@ from aiohttp_jinja2 import template as view
 
 class Login(View):
     @view('user/login.html')
-    async def get(self) -> None:
-        return {}
+    async def get(self) -> dict:
+        return {'location': 'login'}
 
     @view('user/login.html')
-    async def post(self) -> None:
-        return
+    async def post(self) -> dict:
+        return {'location': 'login'}
+
+
+class Registration(View):
+    @view('user/new.html')
+    async def get(self) -> dict:
+        return {'location': 'registration'}
+
+    @view('user/new.html')
+    async def post(self) -> dict:
+        return {'location': 'registration'}
+
+
+class RecoverPassword(View):
+    @view('user/recover_password.html')
+    async def get(self) -> dict:
+        return {'location': 'recover_password'}
+
+    @view('user/recover_password.html')
+    async def post(self) -> dict:
+        return {'location': 'recover_password'}
 
 routes = {
-    "login": Login
+    "login": Login,
+    "register": Registration,
+    "recover-password": RecoverPassword
 }
