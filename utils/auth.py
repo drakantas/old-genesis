45f9fb2dab3131c4745d5c0b8038ca61762dac3e
Draku <vladimir.ibazeta@gmail.com>
2017-10-19 17:12:26 -0500

    Flag para identificar si un estudiante tiene un proyecto ya creado

--- utils/auth.py
+++ utils/auth.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from aiohttp.web import Request
 from aiohttp_session import get_session
 
@@ -32,7 +33,20 @@ async def get_auth_data(request: Request) -> dict:
                    rol_usuario.revisar_proyectos as perm_revisar_proyectos,
                    rol_usuario.autorizar_estudiantes as perm_autorizar_estudiantes,
                    rol_usuario.mantener_usuarios as perm_mantener_usuarios,
-                   rol_usuario.mantener_roles as perm_mantener_roles
+                   rol_usuario.mantener_roles as perm_mantener_roles,
+                   (SELECT true
+                    FROM proyecto
+                    RIGHT JOIN integrante_proyecto
+                            ON integrante_proyecto.proyecto_id = proyecto.id AND
+                               integrante_proyecto.usuario_id = usuario.id AND
+                               integrante_proyecto.aceptado = true
+                    WHERE proyecto.ciclo_acad_id = (SELECT id FROM ciclo_academico
+                                                    WHERE ciclo_academico.fecha_comienzo <= $2 AND
+                                                          ciclo_academico.fecha_fin >= $2 AND
+                                                          ciclo_academico.escuela = usuario.escuela
+                                                    LIMIT 1) AND
+                          integrante_proyecto.usuario_id = usuario.id
+                   ) as has_project
             FROM usuario
             LEFT JOIN rol_usuario
                    ON rol_usuario.id = usuario.rol_id
@@ -40,6 +54,6 @@ async def get_auth_data(request: Request) -> dict:
                   deshabilitado != true
         '''
         stmt = await connection.prepare(query)
-        user = await stmt.fetchrow(int(session['id']))
+        user = await stmt.fetchrow(int(session['id']), datetime.utcnow())
 
         return user
